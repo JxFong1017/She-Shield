dc76704eca9f507b5438e4731c35d9337b99453c
Tan Yun Yu <tanyunyu33@gmail.com>
2026-01-02 15:59:23 +0800

    upload photo to online location

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-storage")
 
     // 3. Google Play Services (Auth, Location, and Maps)
     implementation("com.google.android.gms:play-services-auth:21.3.0")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file
 plugins {
-    // Standard Android plugins using the 'id' syntax for better compatibility
-    id("com.android.application") version "8.9.1" apply false
-    id("com.android.library") version "8.9.1" apply false
+    // Reference the catalog using the alias defined in [plugins] section of toml
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 
-    // Firebase Google Services Plugin - using version 4.4.2 as requested
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    // Firebase Google Services
+    alias(libs.plugins.google.gms.google.services) apply false
 }
