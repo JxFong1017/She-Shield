197dc2d3a24348e80618891953897984bbb8bdef
1111-wen <yiwenlai0502@gmail.com>
2026-01-04 22:20:48 +0800

    home page sos button

--- app/src/main/java/com/example/grpassignment/SheShieldApplication.java
+++ app/src/main/java/com/example/grpassignment/SheShieldApplication.java
@@ -1,12 +1,16 @@
 package com.example.grpassignment;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.util.Log;
 
 import com.google.firebase.FirebaseApp;
 
 public class SheShieldApplication extends Application {
     private static final String TAG = "SheShieldApp";
+    public static final String SOS_CHANNEL_ID = "SosServiceChannel";
 
     @Override
     public void onCreate() {
@@ -16,5 +20,19 @@ public class SheShieldApplication extends Application {
 
         // This line is crucial. It initializes Firebase for the entire app.
         FirebaseApp.initializeApp(this);
+
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    SOS_CHANNEL_ID,
+                    "SOS Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
     }
 }
